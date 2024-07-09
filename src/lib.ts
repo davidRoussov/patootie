f7708f589e4d5d7dd3f904eb7249393f3abd3019
David Roussov <david.roussov@protonmail.com>
2024-07-09 16:35:05 +1000

    obtain output from parversion

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,31 @@
+import { spawn } from 'child_process';
 import puppeteer from 'puppeteer';
 
+const parverse = async (html: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const parversion = '/Users/david/projects/parversion';
+
+    let output = '';
+
+    const cargoProcess = spawn('cargo', ['run'], { cwd: parversion });
+
+    cargoProcess.stdin.write(html + '\n');
+    cargoProcess.stdin.end();
+
+    cargoProcess.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    cargoProcess.stderr.on('data', (error) => {
+      console.error('error', error.toString());
+    });
+
+    cargoProcess.on('close', (code) => {
+      console.log(`Parversion exited with code: ${code}`);
+      resolve(output);
+    });
+  });
+};
 
 export const go = async (url: string): Promise<void> => {
 
@@ -19,6 +45,13 @@ export const go = async (url: string): Promise<void> => {
   const html = await page.content();
   console.log('html', html);
 
+
+
+
+  const output = await parverse(html);
+
+
+  console.log(output);
 
 
 
