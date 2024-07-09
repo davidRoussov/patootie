a7d2d8dd5263d698fc4d6cbbd488f3afbe3575a1
David Roussov <david.roussov@protonmail.com>
2024-07-09 16:01:41 +1000

    get page html using puppeteer

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,28 @@
+import puppeteer from 'puppeteer';
+
+
 export const go = async (url: string): Promise<void> => {
-  console.log('go', url);
+
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+
+
+  //const screenshot = await page.screenshot({ encoding: 'base64' });
+
+
+  const html = await page.content();
+  console.log('html', html);
+
+
+
+
+
+  await browser.close();
 };
+
